48eeed3a7b1995a20d4188f9c05d5f43db12eaf5
NathanBernard <nbx-off@outlook.fr>
2026-05-15 15:17:37 +0200

    add Hashset hash function

--- Hashset.js
+++ Hashset.js
@@ -6,6 +6,17 @@ export class HashSet {
     this.size = 0;
   }
 
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+
   length() {
     return this.size;
   }
