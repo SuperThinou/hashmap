d307ad988c3caf929464ba92ad1e1d2f632216f6
NathanBernard <nbx-off@outlook.fr>
2026-05-15 15:08:35 +0200

    add test for all others functions

--- main.js
+++ main.js
@@ -14,24 +14,50 @@ test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
 test.set("lion", "golden");
-
 // console.log(test.buckets);
 
 // Overwrite values test
+
 test.set("apple", "green");
 test.set("banana", "green");
 // console.log(test.buckets);
 
 // Load factor exceedance test
+
 test.set("moon", "silver");
 test.set("car", "black");
 test.set("sky", "blue");
 test.set("grass", "green");
-
 // console.log(test.buckets);
 
-// Remove function test
-test.remove("moon");
-test.remove("apple");
+// set function test with the new hashes
 
+test.set("moon", "grey");
+test.set("car", "grey");
+// console.log(test.buckets)
+
+// Remove function test
+
+test.remove("grass");
+test.remove("apple");
+console.log(test.buckets);
+
+// get function test
+
+console.log(test.get("banana"));
+console.log(test.get("kiwi"));
+
+// length function test
+console.log(test.length());
+
+// keys function test
+console.log(test.keys());
+
+// values function test
+console.log(test.values());
+
+// entries function test
+console.log(test.entries);
+
+test.clear();
 console.log(test.buckets);
